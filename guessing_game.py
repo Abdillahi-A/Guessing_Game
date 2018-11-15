47aa65a24cd3c41658547dc71d15703315a8391c
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2018-11-15 17:02:35 +0000

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -1,28 +1,40 @@
 import random
 
-number = random.randint(1,10)
-
 name = input("Hello, what is your name ? ")
 
-print(f"Well, {name} I'm thinking of a number between 1 and 10." )
+print(f"Well, {name} I'm thinking of a number between 1 and 20." )
 
-guess = int(input("Take a guess. "))
+play = 'yes'
 
-guess_count = 1
+while play.lower()[0] == 'y':
 
-while guess_count <5:
-    if guess > number:
-        print("Your guess is too high.")
-        guess = int(input("Take a guess. "))
+    secretnumber = random.randint(1,20)
+    
+    guess = 0
+    
+    guess_count = 0
+      
+    while guess_count <5 and guess != secretnumber:
+        guess = int(input("Take a guess "))
         guess_count += 1
-    elif guess< number:
-        print("Your guess is too low.")
-        guess = int(input("Take a guess. "))
-        guess_count += 1
-    elif guess == number:
-        print(f"Congrats {name} you guessed correct in {guess_count} guesses!")
-        break
+        if guess < secretnumber:
+            print("Your guess is too low. ")
+        elif guess > secretnumber:
+            print("Your guess is too high.")
+                      
+    if guess != secretnumber:   
+         print(f"Sorry you are out of guesses. The number I was thinking of was {secretnumber}.")
+    else:
+        print(f"Congrats, you guessed correct in {guess_count} guesses!")
+        
+    play = input("Do you want to play again? Y or N ? ")
+    while play.lower()[0] not in ['y','n']:
+        play = input("Please type 'Y' to play again or 'N' to exit ? ")
+        
+    if play.lower()[0] == 'y':
+        print("Cool! Let's play again!")
         
 else:
-    print(f"Sorry you are out of guesses. The number I was thinking of was {number}.")
+    print("That's too bad. Come back again later. Bye!")
     
+        
