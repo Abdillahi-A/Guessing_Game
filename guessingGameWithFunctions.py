426531909421f69a5bebe5702729e1dc1214b7db
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2019-01-07 21:49:33 +0000

    Update guessingGameWithFunctions.py

--- guessingGameWithFunctions.py
+++ guessingGameWithFunctions.py
@@ -4,54 +4,49 @@ def generateRandomNumber():
     randomNumber = random.randint(1,20)
     return randomNumber
 
-def checkUserInputtedNumber(guess):
+def getUserInput(numberOfGuesses, randomNumber):
     while True:
         try:
-            guess = int(guess)
-            break
+            guess = int(input("Take a guess\n"))
+            numberOfGuesses +=1
+            if guess != randomNumber:
+                print(f"You have {5 - numberOfGuesses} guesses left.")
+            return guess, numberOfGuesses
         except ValueError:
-            guess = input("Oops that didn't work. Please make sure you choose a valid number\n")
-    return guess
+            print("Oops, that didn't work. Make sure you input a number.\n")
 
-def checkIfGuessIsCorrect(guess,randomNumber, number_of_guesses):
-        if guess == randomNumber:
-            print(f"Congrats, you guessed correctly in {number_of_guesses} guesses! The number I was thinking of was {randomNumber}.")
-        
-        elif guess < randomNumber and number_of_guesses <5:
-            guess = input(f"Your guess is too low. You have {5 - number_of_guesses} guesses left Try again.\n")
-            guess = checkUserInputtedNumber(guess)
-            number_of_guesses += 1
-            checkIfGuessIsCorrect(guess,randomNumber, number_of_guesses)
+def checkGuess(randomNumber, guess, numberOfGuesses):
+    while guess != randomNumber and numberOfGuesses <5:
+        if guess < randomNumber:
+            print("Your guess is too low. Try Again.\n")
+            guess, numberOfGuesses = getUserInput(numberOfGuesses,randomNumber)
+        elif guess > randomNumber:
+            print("Your guess is too high. Try Again.\n")
+            guess, numberOfGuesses = getUserInput(numberOfGuesses,randomNumber)
+    
+    if guess == randomNumber:
+        print(f"Congrats, the number I was thinking of was {randomNumber}.")
+    else:
+        print(f"You are out of guesses. The number I was thinking of was {randomNumber}.")
 
-        elif guess > randomNumber and number_of_guesses <5:
-            guess = input("Your guess is too high. Try again.\n")
-            guess = checkUserInputtedNumber(guess)
-            number_of_guesses += 1
-            checkIfGuessIsCorrect(guess,randomNumber, number_of_guesses)
-
-        else:
-            print(f"Sorry you are out of guesses. The number I was thinking of was {randomNumber}.")
-
-def play_again():
-    playAgain = input("Do you want to play again? Type 'Y' to play again or 'N' to exit.\n").upper()
-    while playAgain.upper() not in ['Y','N']:
-        playAgain = input("Please type 'Y' to play again or 'N' to exit.\n").upper()
-
-    if playAgain == 'Y':
+def playAgain():
+    playAgain = input("\nDo you want to play again? Type 'Y' to play again or 'N' to quit.\n")
+    while playAgain.upper() not in ['Y', 'N']:
+        playAgain = input("Type 'Y' to play again or 'N' to quit.\n").upper()
+    if playAgain.upper()[0] == 'Y':
+        print("Cool let's play again.\n")
         return True
-        
+    else:
+        print("That's too bad. Come back again later. Bye!")
+    
 def main():
     while True:
         randomNumber = generateRandomNumber()
-        guess = input("I am thinking of a number between 1 and 20. What number am I thinking of?\n")
-        guess = checkUserInputtedNumber(guess)
-        number_of_guesses = 1
-        checkIfGuessIsCorrect(guess,randomNumber, number_of_guesses)
-        if not play_again():
-            print("That's too bad. Come back again later. Bye!")
+        print("I am thinking of a random integer between 1 and 20. What number am I thinking of?\n")
+        numberOfGuesses = 0
+        guess, numberOfGuesses = getUserInput(numberOfGuesses,randomNumber)
+        checkGuess(randomNumber, guess, numberOfGuesses)
+        if not playAgain():
             break
-        else:
-            print("Cool! Let's play again!\n")
-    
 
 main()
